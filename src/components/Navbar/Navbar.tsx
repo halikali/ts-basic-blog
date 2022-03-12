da844186f29422aacac2d5adb24b77e572013b01
halikali <tunchalikali@gmail.com>
2022-03-12 04:05:30 +0300

    add logo for brand

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,12 @@
 import "./Navbar.scss";
+import logo from "assets/main-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} />
+    </div>
+  );
 };
 
 export default Navbar;
