35e81d7d9857137185821aa7b0dcebb6c54ba9c8
halikali <tunchalikali@gmail.com>
2022-03-12 23:08:13 +0300

    complete NavListItem component

---
+++ src/components/NavList/NavListItem/NavListItem.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+
+interface INavLıstItemProps {
+  customClass?: string;
+  to: string;
+  iconName: any;
+  pageName: string;
+}
+
+const NavListItem = (props: INavLıstItemProps) => {
+  const { to, iconName, customClass, pageName } = props;
+  return (
+    <NavLink className={`navlist__link ${customClass && customClass} `} to={to}>
+      <FontAwesomeIcon icon={iconName} className={"navlist__icon"} />
+      {pageName}
+    </NavLink>
+  );
+};
+
+export default NavListItem;
