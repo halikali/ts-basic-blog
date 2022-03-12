a83234a451ab950cc81d3e91add3126a7fba6d18
halikali <tunchalikali@gmail.com>
2022-03-13 01:36:22 +0300

    complete footer

--- src/components/Footer/Foter.tsx
+++ src/components/Footer/Foter.tsx
@@ -1,7 +1,13 @@
-import ".;/Footer.scss";
+import "./Footer.scss";
 
 const Foter = () => {
-  return <div>Foter</div>;
+  return (
+    <div className="footer">
+      <p className="footer__text">
+        Exeption Error &copy; {new Date().getFullYear()} All Right Reserved
+      </p>
+    </div>
+  );
 };
 
 export default Foter;
