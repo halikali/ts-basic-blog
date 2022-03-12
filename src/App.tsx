486d1edbc4406826fd316370c1b686de57da69a4
halikali <tunchalikali@gmail.com>
2022-03-12 03:44:10 +0300

    add routes and dispatch redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router";
+
+import { getDate } from "store/actions/exampleAction";
+import routes from "./routes";
+import "./App.scss";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDate());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        {routes.map((route) => (
+          <Route path={route.path} element={route.page} key={route.name} />
+        ))}
+      </Routes>
     </div>
   );
 }
