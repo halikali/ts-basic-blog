bdaf2224f2aec3400f3b45eeeee4c98be890224e
halikali <tunchalikali@gmail.com>
2022-03-13 22:01:43 +0300

    dispatch get posts reducer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
 import Card from "components/Card";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "store/actions/postActions";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
+
+
   return (
     <div className="container">
       <Card />
