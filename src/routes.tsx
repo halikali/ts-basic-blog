9dc66270aed2fdaf2300ca06b7ac083027c8259d
halikali <tunchalikali@gmail.com>
2022-03-13 12:37:57 +0300

    add route for create post page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+import CreatePost from "pages/CreatePost/CreatePost";
 import Home from "pages/Home";
 import PostDetail from "pages/PostDetail/PostDetail";
 
@@ -6,6 +7,11 @@ const routes = [
     path: "/details",
     name: "postDetail",
     page: <PostDetail />,
+  },
+  {
+    path: "/create-post",
+    name: "createPost",
+    page: <CreatePost />,
   },
   {
     path: "/",
