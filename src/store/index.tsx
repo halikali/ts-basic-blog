ce4be75945db3d8ddbbd4c1889033fac84bb68fe
halikali <tunchalikali@gmail.com>
2022-03-13 22:02:34 +0300

    import the reducers

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 
 import dateReducer from "./reducers/dateReducer";
+import getPostsReducer from "./reducers/getPostsReducer";
+import postReducer from "./reducers/postReducer";
 
 const rootReducer = combineReducers({
+  post: postReducer,
+  posts: getPostsReducer,
   dateReducer,
 });
 
