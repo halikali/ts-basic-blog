b0022e63eaa1f73e6a264db564b5b44da44afb5a
halikali <tunchalikali@gmail.com>
2022-03-13 22:19:17 +0300

    map the card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,21 @@
 import Card from "components/Card";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "store/actions/postActions";
+import { IPost } from "types/postTypes";
+import { IRootState } from "types/reducerTypes";
 
 const Home = () => {
   const dispatch = useDispatch();
-  
+  const { posts } = useSelector((state: IRootState) => state.allPosts);
+
   useEffect(() => {
     dispatch(getPosts());
   }, []);
 
-
   return (
     <div className="container">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {posts && posts.map((post: IPost) => <Card key={post.id} />)}
     </div>
   );
 };
