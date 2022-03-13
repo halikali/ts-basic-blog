7264c83fb182536dea29909a3f3cefa8b82bec7c
halikali <tunchalikali@gmail.com>
2022-03-13 22:01:09 +0300

    clean unnecessary comments row

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBalLJcOmzclkPpBLGGvBb91XY3p11QG_4",
   authDomain: "ts-basic-blog.firebaseapp.com",
@@ -19,7 +15,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export { db };
