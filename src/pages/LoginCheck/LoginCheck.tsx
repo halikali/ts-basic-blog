1b7dd91ae8c7e43ac9e3051dd408d55c05191a78
halikali <tunchalikali@gmail.com>
2022-03-15 15:20:39 +0300

    create loginCheck page

---
+++ src/pages/LoginCheck/LoginCheck.tsx
@@ -0,0 +1,12 @@
+import Login from "components/Login";
+import "./LoginCheck.scss";
+
+const LoginCheck = () => {
+  return (
+    <div className="container">
+      <Login />
+    </div>
+  );
+};
+
+export default LoginCheck;
